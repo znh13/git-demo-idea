896c5944019c83d7a37886cc36e17766c25075ab
mikasa <mikasa@znh.com>
2021-09-11 22:48:37 +0800

    commit by idea second

--- src/main/java/com/huang/git/HelloGit.java
+++ src/main/java/com/huang/git/HelloGit.java
@@ -3,5 +3,6 @@ package com.huang.git;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
+        System.out.println("stop the world");
     }
 }
