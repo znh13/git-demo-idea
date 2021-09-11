c3e5049544afbfd339d255027cc019e2b33f0037
znh13 <87868008+znh13@users.noreply.github.com>
2021-09-11 23:38:37 +0800

    commit by qzy

--- src/main/java/com/huang/git/HelloGit.java
+++ src/main/java/com/huang/git/HelloGit.java
@@ -4,7 +4,7 @@ public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("stop the world");
-        System.out.println("邱振宇");
+        System.out.println("邱振宇 is gay");
         System.out.println("good morning everyone");
         System.out.println("hot-fix line");
         System.out.println("master line");
